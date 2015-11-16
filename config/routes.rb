08d737077f3fa5da8fa6d89c4c6c3dc4ad71775a
jen pengelly <jgpengelly@gmail.com>
2015-11-16 17:09:26 -0500

    create nested route for notecards. create views for notecards with pseudo-code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root to: 'home#index'
+  resources :boards do
+    resources :notecards
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
