a2d041dcaa1ca7025168fbdf7c771116359bdff0
jen pengelly <jgpengelly@gmail.com>
2015-11-17 10:34:26 -0500

    updated boards and notecards controllers. Updated views for layout and boards index

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,37 @@ class BoardsController < ApplicationController
     @boards = Board.all
   end
 
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.create!(board_params.merge(user:current_user))
+    redirect_to @board
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.update!(board_params.merge(user:current_user))
+    redirect_to @board
+  end
+
+  def destroy
+    @board = current_user.boards.find(params[:id]).destroy
+    redirect_to boards_path
+  end
+
+#Stong Params
+private
+  def board_params
+    params.require(:board).permit(:title, :status)
+  end
+
 end
--- app/controllers/notecards_controller.rb
+++ app/controllers/notecards_controller.rb
@@ -1,10 +1,18 @@
 class NotecardsController < ApplicationController
-
+  before_action :set_board, only: [:new, :create, :edit, :update, :destroy]
+  
   def index
-    @notecards = Notecard.all
+    @board = Board.find(params[:board_id])
+    @notecards = @board.notecards.all
   end
 
   def show
+    @board = Board.find(params[:board_id])
+
+    @notecard =
+  end
+
+  def new
   end
 
   def create
@@ -13,11 +21,21 @@ class NotecardsController < ApplicationController
   def edit
   end
 
+  def update
+  end
+
   def destroy
   end
 
+# Strong Params
 private
+
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+
   def notecard_params
+    params.require(:notecard).permit(:task, :date, :status, :accomplished, :to_do, :roadblock)
   end
 
 end
