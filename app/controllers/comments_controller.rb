ecac10185aed23a8bde9276a28bb54bdae469f47
jen pengelly <jgpengelly@gmail.com>
2015-12-25 11:49:12 -0500

    Update Gemfile with better_error_messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,8 @@
 class CommentsController < ApplicationController
 before_action :set_notecard, only: [:create, :destroy]
-before_action :set_comment, only: [:edit, :update]
-
-  def new
-    @comment = Comment.new
-  end
 
   def create
     @comment = @notecard.comments.create!(comment_params.merge(user:current_user))
-    redirect_to board_notecard_path(@notecard)
-  end
-
-  def edit
-  end
-
-  def update
-    @comment.update(comment_params)
     redirect_to board_notecard_path(@notecard)
   end
 
@@ -28,10 +15,6 @@ before_action :set_comment, only: [:edit, :update]
 private
   def set_comment
     @comment = Comment.find(params[:id])
-  end
-
-  def set_notecard
-    @notecard = Notecard.find(params[:id])
   end
 
   def comment_params
